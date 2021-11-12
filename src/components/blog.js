25636a7009734565857b5d948ab3502358eacda1
KScriven <kerryn.scriven@bbc.co.uk>
2021-11-12 14:59:35 +0000

    Remove jest

--- src/components/blog.js
+++ src/components/blog.js
@@ -3,11 +3,11 @@ import { useQuery } from '@apollo/client';
 import GET_MY_DATA from '../queries';
 
 function Blog() {
-  const { loading, data, error } = useQuery(GET_MY_DATA);
+  // const { loading, data, error } = useQuery(GET_MY_DATA);
 
-  if (loading) return "Loading...";
+  // if (loading) return "Loading...";
 
-  if (error) return (<pre>Come back later we are taking a break</pre>);
+  // if (error) return (<pre>Come back later we are taking a break</pre>);
 
   return (
     <div data-testid="blog-testid">
@@ -15,7 +15,7 @@ function Blog() {
         <h2>Notes to Self</h2>
       </header>
       <p>ToDo: GraphQL Get some blogs setup here for reading</p>
-      <p>Existing data so far {data}</p>
+
     </div>
   );
 }
