c970e1c10cb188735bfd97c131d4f4e05de77f3f
KScriven <kerryn.scriven@bbc.co.uk>
2021-11-14 12:59:21 +0000

    Update to blogs

--- src/components/blog.js
+++ src/components/blog.js
@@ -1,26 +1,24 @@
 import React from 'react';
-import { useQuery } from '@apollo/client';
-import { GET_MY_BLOGS } from '../queries';
+// import { useQuery } from '@apollo/client';
+// import { GET_MY_BLOGS } from '../queries';
 
 function Blog() {
-  const { loading, data, error } = useQuery(GET_MY_BLOGS);
+  // const { loading, data, error } = useQuery(GET_MY_BLOGS);
 
-  if (loading) return "Loading...";
+  // if (loading) return "Loading...";
 
-  if (error) return (<pre>Come back later we are taking a break</pre>);
-
-  console.log(data);
+  // if (error) return (<pre>Come back later we are taking a break</pre>);
 
   return (
     <div data-testid="blog-testid">
       <header>
         <h2>Welcome to my Blogs to Self</h2>
       </header>
-      <div>
+      {/* <div>
         {data && data.map((blog, idx) => (
           <><span key={idx}></span><h3>{blog.intro}</h3></>
         ))}
-      </div>
+      </div> */}
       {/* <p>{data}</p> */}
     </div>
   );
